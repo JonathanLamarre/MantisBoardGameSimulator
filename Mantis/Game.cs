971bb92f1e11e997464d84ecf52d0c3e879db949
Jonathan Lamarre <jlamarre@genetec.com>
2024-04-01 00:11:28 -0400

    Cleanup
    Cleanup

---
+++ Mantis/Game.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Mantis.Logics;
+
+namespace Mantis
+{
+    public class Game
+    {
+        private const int SCORE_TO_WIN = 10;
+
+        public ILogic Winner { get; }
+
+        public int WinnerPosition { get; }
+
+        public Game(ImmutableList<ILogic> logics)
+        {
+            var deck = new Deck();
+            ImmutableList<Player> players = logics.Select(x => new Player(deck, x)).ToImmutableList();
+            int indexOfPlayerPlaying = 0;
+
+            while (true)
+            {
+                if (deck.Cards.Count == 0)
+                {
+                    Player winningPlayer = players
+                        .OrderBy(x => x.Score)
+                        .ThenBy(x => x.Tank.Aggregate(0, (i, y) => i + y.Value))
+                        .Last();
+
+                    int indexOfWinningPlayer = players.IndexOf(winningPlayer);
+                    Winner = logics[indexOfWinningPlayer];
+                    WinnerPosition = indexOfWinningPlayer;
+
+                    break;
+                }
+
+                Player activePlayer = players[indexOfPlayerPlaying];
+                activePlayer.PlayTurn(deck, players);
+
+                if (activePlayer.Score >= SCORE_TO_WIN)
+                {
+                    int indexOfWinningPlayer = players.IndexOf(activePlayer);
+                    Winner = logics[indexOfWinningPlayer];
+                    WinnerPosition = indexOfWinningPlayer;
+
+                    break;
+                }
+
+                indexOfPlayerPlaying = (indexOfPlayerPlaying + 1) % players.Count;
+            }
+        }
+    }
+}
--- Mantis/Program.cs
+++ Mantis/Program.cs
@@ -70,7 +70,7 @@ namespace Mantis
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            Parallel.For(0, 10000000, _ =>
+            Parallel.For(0, 1000000, _ =>
             {
                 var logics = new List<ILogic>();
                 var random = new Random();
@@ -102,53 +102,6 @@ namespace Mantis
             }
 
             Console.WriteLine($"{stopwatch.Elapsed}");
-        }
-    }
-
-    public class Game
-    {
-        private const int SCORE_TO_WIN = 10;
-
-        public ILogic Winner { get; }
-
-        public int WinnerPosition { get; }
-
-        public Game(ImmutableList<ILogic> logics)
-        {
-            var deck = new Deck();
-            ImmutableList<Player> players = logics.Select(x => new Player(deck, x)).ToImmutableList();
-            int indexOfPlayerPlaying = 0;
-
-            while (true)
-            {
-                if (deck.Cards.Count == 0)
-                {
-                    Player winningPlayer = players
-                        .OrderBy(x => x.Score)
-                        .ThenBy(x => x.Tank.Aggregate(0, (i, y) => i + y.Value))
-                        .Last();
-
-                    int indexOfWinningPlayer = players.IndexOf(winningPlayer);
-                    Winner = logics[indexOfWinningPlayer];
-                    WinnerPosition = indexOfWinningPlayer;
-
-                    break;
-                }
-
-                Player activePlayer = players[indexOfPlayerPlaying];
-                activePlayer.PlayTurn(deck, players);
-
-                if (activePlayer.Score >= SCORE_TO_WIN)
-                {
-                    int indexOfWinningPlayer = players.IndexOf(activePlayer);
-                    Winner = logics[indexOfWinningPlayer];
-                    WinnerPosition = indexOfWinningPlayer;
-
-                    break;
-                }
-
-                indexOfPlayerPlaying = (indexOfPlayerPlaying + 1) % players.Count;
-            }
         }
     }
 }
